31d45cd1b911ef031e6fe77c024d440887d3126a
MirrichD <2693970161@qq.com>
2024-10-22 01:34:10 +0800

    修复切换排序的BUG

--- src/pages/Works.vue
+++ src/pages/Works.vue
@@ -150,8 +150,8 @@ export default {
 
   watch: {
     sortOption(newSortOptionSetting) {
-      const localSortOption = this.$q.localStorage.getItem("sortOption");
-      if (localSortOption.order !== newSortOptionSetting.order & localSortOption.sort !== newSortOptionSetting.sort) {
+      const localSortOption = this.$q.localStorage.getItem("sortOption", {});
+      if (localSortOption.label !== newSortOptionSetting.label) {
         this.$q.localStorage.set("sortOption", newSortOptionSetting);
         this.reset();
       }
